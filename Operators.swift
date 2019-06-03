69da5fa76521b967d594bfb737670d340e241813
Moshé Marciano <49877926+marsdev26@users.noreply.github.com>
2019-06-03 21:01:30 +0300

    Create Operators.swift

---
+++ Operators.swift
@@ -0,0 +1,64 @@
+// Binary OperatorsBinary
+
+let height: Double = 12 // in ft
+let width: Double = 10 // in ft
+
+let area = height * width // in sq feet
+
+// 1 sq meter = 1 sq foot / 10.764
+
+let areaInMeters = area/10.764
+
+// Equals Operator
+
+let string1 = "Hello!"
+let string2 = "Hello!"
+let string3 = "Hello"
+
+string1 == string2
+string1 == string3
+
+// Not equal
+
+string1 != string2
+string1 != string3
+
+// Greater than
+
+1 > 2
+2 > 1
+
+"a" > "b"
+
+
+// Operator Precedence
+
+var x = 100 + 100 - 5 * 2 / 3 % 7
+
+25 - (5 * 2) + 5
+5 + 5 - (5 * 2)
+
+// Unary Operators
+
+var levelScore = 0
+// levelScore = levelScore + 1
+levelScore += 1
+levelScore -= 1
+levelScore
+
+// NOT Operator
+
+let on = true
+let off = !on
+
+
+let a = 4
+let b = a != 10
+
+
+
+
+
+
+
+
