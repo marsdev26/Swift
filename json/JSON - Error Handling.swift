65c7f8b852d3e0e8abfd623f1bbb2ab0bd79789f
Moshé Marciano <49877926+marsdev26@users.noreply.github.com>
2019-08-25 22:07:52 +0300

    Create JSON - Error Handling.swift

---
+++ json/JSON - Error Handling.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let json = """
+{
+"id": 10
+}
+""".data(using: .utf8)
+
+struct User: Codable {
+    let id: Int
+    let name: String
+}
+
+let decoder = JSONDecoder()
+
+do {
+    let user = try decoder.decode(User.self, from: json)
+} catch {
+    print(error)
+}
