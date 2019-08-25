09d1a288da8d7e96051d0ac4d690b8d39c74c4d4
Moshé Marciano <49877926+marsdev26@users.noreply.github.com>
2019-08-25 22:05:48 +0300

    Create JSON - Codable.swift

---
+++ json/JSON - Codable.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+let json = """
+{
+    "name": "Pasan",
+    "id": 1,
+    "role": "Teacher",
+    "start_date": "April 2012"
+
+}
+""".data(using: .utf8)!
+
+struct Employee: Codable {
+    let name: String
+    let id: Int
+    let role: String
+    let startDate: String
+ 
+}
+
+
+let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
+let employee = try! decoder.decode(Employee.self, from: json)
+print(employee)
+
+employee.name
+
+let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
+let encodedEmployee = try! encoder.encode(employee)
+print(encodedEmployee.stringDescription)
